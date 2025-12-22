b596757056d11a229955334377e9ee1fb634c10f
hananzh399 <hananzh399@gmail.com>
2025-12-22 18:03:47 +0500

    add these two modified files of style.css and search box.js

---
+++ searchbox.js
@@ -0,0 +1,112 @@
+   //search values and results
+   
+   function ValueAndResults(){
+    if (sval === 'paytrack'){
+                window.location.href = "https://hananzh399.github.io/School-website-try/"
+            }
+            else if (sval === 'fa'){
+                 window.location.href = "https://www.youtube.com/@FazalAcademy"
+            }else if(sval === "news"){
+                document.querySelector('.h-latest-news').scrollIntoView({
+                    behavior: "smooth",
+                });
+            }else if(sval === ''){
+                console.log("Empty");
+            }else if(sval === ''){
+                console.log("Empty");
+            }else{
+                alert(`No result found for : ${sval}`);
+            }
+            
+        }
+   
+   
+   //search bar for Desktop// 
+    
+    let search = document.querySelector('.search');
+    let clean = document.querySelector('.searchclean');
+    let seco = document.getElementById('search-container');
+    let sechea = document.getElementById('second-header');
+    let seaIcon = document.querySelector('.search-open');
+    let cleIcon = document.querySelector('.search-close');
+    let searchBtn = document.querySelector('.clear .fa-search');
+    let input = document.querySelector('.search-input');
+
+
+    search.onclick = function(){
+        seco.classList.toggle('active');
+        if (seco.classList.contains('active')) {
+       seaIcon.style.visibility = "collapse";
+       cleIcon.style.visibility = "visible";
+       input.focus();
+    };
+        document.querySelector('.search-close').addEventListener('click', ()=>{
+            seco.classList.remove('active');
+            seaIcon.style.visibility = "visible";
+       cleIcon.style.visibility = "collapse";
+       input.value = '';
+        });
+    };
+    clean.onclick = function(){
+        document.getElementById('search').value = '';
+    }
+
+    
+     input.onkeypress = function(event) {
+        if (event.key === 'Enter') {
+            sval = input.value.toLowerCase().replace(/\s+/g, '');
+          ValueAndResults();
+            input.value = '';
+            
+    };
+};
+
+searchBtn.addEventListener('click', ()=>{
+    sval = input.value.toLowerCase().replace(/\s+/g, '');
+
+   ValueAndResults();
+   document.getElementById('search').value = '';
+});
+
+
+//search bar for mobile//
+
+let mbSearchOpen = document.querySelector('.mb-search');
+let mbSearchContainer = document.querySelector('.mb-search-container');
+let mbSearchClose = document.querySelector('.mb-search-close');
+let mbSearchResult = document.querySelector('.mb-search-result');
+let mbSearchClear = document.querySelector('.mb-search-clear');
+let ip = document.querySelector('.mb-serach-input');
+
+ mbSearchOpen.addEventListener('click', ()=>{
+    mbSearchContainer.style.visibility = "visible";
+    let verticalDevider1 = document.querySelector('.vartical-devider1');
+    let verticalDevider12 = document.querySelector('#vartical-devider12');
+    verticalDevider1.style.visibility = "hidden";
+    verticalDevider12.style.visibility = "hidden";
+    ip.focus();
+ });
+
+ mbSearchClose.addEventListener('click', ()=>{
+    mbSearchContainer.style.visibility = "hidden";
+    ip.value = '';
+ });
+
+ mbSearchResult.addEventListener('click', ()=>{
+    sval = ip.value.toLowerCase().replace(/\s+/g, '');
+    ValueAndResults();
+    ip.value = '';
+ });
+
+ mbSearchClear.addEventListener('click', ()=>{
+    ip.value = '';
+ });
+
+ ip.onkeypress = function(event) {
+        if (event.key === 'Enter') {
+            sval = ip.value.toLowerCase().replace(/\s+/g, '');
+          
+            ip.value = '';
+            ValueAndResults();
+    };
+};
